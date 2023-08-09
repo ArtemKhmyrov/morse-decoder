7502f79519e4e1782c1c44afed52af249478b095
Хмыров А.М <a.hmyrov-saratov@mail.ru>
2023-08-09 20:56:52 +0400

    init: final

--- src/index.js
+++ src/index.js
@@ -39,8 +39,45 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let str = '';
+    let sybl = '';
+    let item = '';
+    let res = '';
+
+  for (let i = 0; i < expr.length; i += 10) {
+    str = expr.slice(i, i + 10);
+
+    if (str === '**********') {
+        res += ' ';
+        continue;
+    }
+
+    for (let j = 0; j < str.length; j += 2) {
+        sybl = str.slice(j, j + 2);
+
+        if (sybl === '00') {
+            continue;
+        } 
+
+        if (sybl === '10') {
+            item += '.';
+        }   
+
+        if (sybl === '11') {
+            item += '-';
+        }      
+    }
+
+        res += MORSE_TABLE[item];
+        item = '';    
+    }
+
+    return res;
 }
 
 module.exports = {
     decode
 }
+
+
+
